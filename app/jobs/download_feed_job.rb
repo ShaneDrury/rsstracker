21517fd0851a7bf1dabc66a4d865ed4c3856cad7
Shane Drury <shane.drury@freeagent.com>
2018-04-19 21:59:22 +0100

    Download episode info

---
+++ app/jobs/download_feed_job.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'rss'
+
+class DownloadFeedJob < ApplicationJob
+  queue_as :default
+
+  def perform(feed_id)
+    feed = Feed.find(feed_id)
+    rss = RSS::Parser.parse(open(feed.url).read, false).items[0..5]
+    rss.each do |result|
+      Episode.find_or_create_by(feed: feed, name: result.title) do |ep|
+        ep.build_fetch_status(status: "NOT_ASKED")
+        ep.save
+      end
+    end
+  end
+end
