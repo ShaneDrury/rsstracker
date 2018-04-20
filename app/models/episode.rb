042a1eeb05e0e6178770eaf8042eb59bc9b56463
Shane Drury <shane.drury@freeagent.com>
2018-04-20 18:09:22 +0100

    Safely display download_link

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,7 +3,7 @@ class Episode < ApplicationRecord
   has_one :fetch_status, as: :fetchable, dependent: :destroy
 
   def download_link
-    "http://localhost:3000/uploads/#{feed_id}/#{fetch_status.url}" if fetch_status.status == "SUCCESS"
+    "http://localhost:3000/uploads/#{feed_id}/#{fetch_status.url}" if fetch_status&.status == "SUCCESS"
   end
 
   def as_json(options={})
