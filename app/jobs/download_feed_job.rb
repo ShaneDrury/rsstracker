ffdd418f0959bb23f750aff1b38f54980cebd7fd
Shane Drury <shane.drury@freeagent.com>
2018-04-20 21:27:45 +0100

    Download thumbnail image

--- app/jobs/download_feed_job.rb
+++ app/jobs/download_feed_job.rb
@@ -1,13 +1,31 @@
 require 'open-uri'
 require 'rss'
 
+BUFFER_SIZE = 8 * 1024
+
 class DownloadFeedJob < ApplicationJob
   queue_as :default
 
   def perform(feed_id)
     feed = Feed.find(feed_id)
-    rss = RSS::Parser.parse(open(feed.url).read, false).items
-    rss.each do |result|
+    rss = RSS::Parser.parse(open(feed.url).read, false)
+
+    thumbnail_url = rss.channel.image.url
+    thumbnail_filename = File.basename(URI(thumbnail_url).path)
+    feed_dir = Rails.root.join('public', 'uploads', feed_id.to_s)
+    FileUtils.mkdir_p feed_dir
+    download_path = feed_dir.join(thumbnail_filename)
+
+    open(thumbnail_url, 'r') do |input|
+      open(download_path, 'wb') do |output|
+        while (buffer = input.read(BUFFER_SIZE))
+          output.write(buffer)
+        end
+      end
+    end
+    feed.update_attributes(image_url: thumbnail_filename)
+
+    rss.items.each do |result|
       Episode.find_or_create_by(feed: feed, name: result.title, guid: result.guid.content) do |ep|
         ep.build_fetch_status(status: 'NOT_ASKED')
         ep.url = result.link
