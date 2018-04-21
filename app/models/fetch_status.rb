0c57b57a7cb33d524ce986ae646389f64b077294
Shane Drury <shane.drury@freeagent.com>
2018-04-21 14:36:04 +0100

    Show percentage if bytes_transferred is non-nil

--- app/models/fetch_status.rb
+++ app/models/fetch_status.rb
@@ -3,6 +3,6 @@ class FetchStatus < ApplicationRecord
   include ActionView::Helpers::NumberHelper
 
   def percentage_fetched
-    number_with_precision(bytes_transferred.to_d / bytes_total.to_d * 100.0, precision: 2) if status == 'LOADING'
+    number_with_precision(bytes_transferred.to_d / bytes_total.to_d * 100.0, precision: 2) if status == 'LOADING' && bytes_transferred.present?
   end
 end
