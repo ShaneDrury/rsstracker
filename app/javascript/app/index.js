246a5f8f5b61a707fbc11dfd1088e446bb281ed2
Shane Drury <shane.drury@freeagent.com>
2018-04-22 17:12:27 +0100

    Prep for React/Redux

--- app/javascript/app/index.js
+++
@@ -1,2 +0,0 @@
-import { myFoo } from "./test"
-myFoo("heasdf");
--- app/javascript/app/test.ts
+++
@@ -1,5 +0,0 @@
-export const myFoo = (foo: string): number => {
-  console.log("this is typescript");
-  console.log(foo)
-  return 123;
-};
