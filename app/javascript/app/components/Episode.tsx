1b8724923dc19e1feb1afc7c1d98ee9da0b4bcdd
Shane Drury <shane.drury@freeagent.com>
2018-04-22 22:03:34 +0100

    Make Episode a card

--- app/javascript/app/components/Episode.tsx
+++ app/javascript/app/components/Episode.tsx
@@ -12,23 +12,40 @@ export const Episode: React.SFC<Props> = ({
   fetchStatus
 }) => (
   <div>
-    <h2 className="title">{name}</h2>
-    <p className="subtitle">{duration}</p>
-    <div className="field">
-      {fetchStatus.status === "SUCCESS" && <a href={fetchStatus.url}>Listen</a>}
-      {(fetchStatus.status === "NOT_ASKED" ||
-        fetchStatus.status === "FAILURE") && (
-        <button
-          className="button is-primary"
-          onClick={() => downloadEpisode(id)}
-        >
-          Download
-        </button>
-      )}
-      {fetchStatus.status === "LOADING" && (
-        <div>Loading {fetchStatus.percentageFetched}%</div>
-      )}
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">{name}</p>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          {description}
+          <br />
+          <time>{duration}</time>
+        </div>
+        <nav className="level is-mobile">
+          <div className="level-left">
+            {fetchStatus.status === "SUCCESS" && (
+              <a className="level-item" href={fetchStatus.url}>
+                <span className="icon is-small">
+                  <i className="fas fa-file-audio" />Listen
+                </span>
+              </a>
+            )}
+            {(fetchStatus.status === "NOT_ASKED" ||
+              fetchStatus.status === "FAILURE") && (
+              <button
+                className="button is-primary"
+                onClick={() => downloadEpisode(id)}
+              >
+                Download
+              </button>
+            )}
+            {fetchStatus.status === "LOADING" && (
+              <div>Loading {fetchStatus.percentageFetched}%</div>
+            )}
+          </div>
+        </nav>
+      </div>
     </div>
-    <p>{description}</p>
   </div>
 );
