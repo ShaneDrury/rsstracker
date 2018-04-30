89f5ca15573a78d3f5b8b555758bf5ed1a80d6f0
Shane Drury <shane.drury@freeagent.com>
2018-04-30 19:52:06 +0100

    Fix switching between Feeds

--- app/javascript/app/components/Feed.tsx
+++ app/javascript/app/components/Feed.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import { RemoteFeed } from "../types/feed";
 
 import { Episodes } from "./Episodes";
-import { updateFeed } from "../modules/feeds/sources";
+import { getFeed, updateFeed } from "../modules/feeds/sources";
 import { Filter } from "../modules/filters";
+import { RemoteData } from "../modules/remoteData";
 
-interface Props extends RemoteFeed {}
+interface Props {
+  feedId: number;
+}
 
 interface State {
   filter: Filter;
+  remoteData: RemoteData<RemoteFeed, string>;
+  feedId?: number;
 }
 
 export class Feed extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      filter: Filter.ALL
+      filter: Filter.ALL,
+      remoteData: {
+        type: "NOT_ASKED"
+      }
     };
     this.handleFilterChange = this.handleFilterChange.bind(this);
   }
@@ -24,48 +32,91 @@ export class Feed extends React.Component<Props, State> {
     this.setState({ filter: event.target.value as Filter });
   }
 
+  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
+    if (nextProps.feedId !== prevState.feedId) {
+      return {
+        feedId: nextProps.feedId,
+        remoteData: {
+          type: "NOT_ASKED"
+        }
+      };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.state.remoteData.type === "NOT_ASKED") {
+      const feedId = this.props.feedId;
+      this._loadAsyncData(feedId);
+    }
+  }
+
+  async _loadAsyncData(feedId: number) {
+    const feed = await getFeed(feedId);
+    this.setState({
+      remoteData: {
+        type: "SUCCESS",
+        data: feed
+      }
+    });
+  }
+
+  async componentDidMount() {
+    const feedId = this.props.feedId;
+    this._loadAsyncData(feedId);
+  }
+
   render() {
-    const { id, name, description, relativeImageLink, updatedAt } = this.props;
-    return (
-      <div className="columns">
-        <div className="column is-one-third">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <img src={relativeImageLink} />
-              </figure>
-            </div>
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content">
-                  <p className="title is-4">{name}</p>
+    if (this.state.remoteData.type === "SUCCESS") {
+      const {
+        id,
+        name,
+        description,
+        relativeImageLink,
+        updatedAt
+      } = this.state.remoteData.data;
+      return (
+        <div className="columns">
+          <div className="column is-one-third">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-1by1">
+                  <img src={relativeImageLink} />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="title is-4">{name}</p>
+                  </div>
                 </div>
+                <div className="content">
+                  {description}
+                  <br />
+                  <time>{updatedAt}</time>
+                </div>
+                <button
+                  className="button is-primary"
+                  onClick={() => updateFeed(id)}
+                >
+                  <i className="fas fa-sync" />&nbsp;Update
+                </button>
+                <select onChange={this.handleFilterChange}>
+                  <option value={Filter.ALL}>All</option>
+                  <option value={Filter.SUCCESS}>Success</option>
+                  <option value={Filter.NOT_ASKED}>Not asked</option>
+                  <option value={Filter.LOADING}>Loading</option>
+                  <option value={Filter.FAILURE}>Failure</option>
+                </select>
               </div>
-              <div className="content">
-                {description}
-                <br />
-                <time>{updatedAt}</time>
-              </div>
-              <button
-                className="button is-primary"
-                onClick={() => updateFeed(id)}
-              >
-                <i className="fas fa-sync" />&nbsp;Update
-              </button>
-              <select onChange={this.handleFilterChange}>
-                <option value={Filter.ALL}>All</option>
-                <option value={Filter.SUCCESS}>Success</option>
-                <option value={Filter.NOT_ASKED}>Not asked</option>
-                <option value={Filter.LOADING}>Loading</option>
-                <option value={Filter.FAILURE}>Failure</option>
-              </select>
             </div>
           </div>
+          <div className="column">
+            <Episodes feedId={id} filter={this.state.filter} />
+          </div>
         </div>
-        <div className="column">
-          <Episodes feedId={id} filter={this.state.filter} />
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div>LOADING</div>;
   }
 }
--- app/javascript/app/components/FeedContainer.tsx
+++ app/javascript/app/components/FeedContainer.tsx
@@ -1,8 +1,5 @@
-import * as React from "react";
+import React from "react";
 import { Feed } from "./Feed";
-import { getFeed } from "../modules/feeds/sources";
-import { RemoteFeed } from "../types/feed";
-import { RemoteData } from "../modules/remoteData";
 
 interface Props {
   match: {
@@ -12,37 +9,6 @@ interface Props {
   };
 }
 
-interface State {
-  remoteData: RemoteData<RemoteFeed, string>;
-}
-
-export class FeedContainer extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      remoteData: {
-        type: "NOT_ASKED"
-      }
-    };
-  }
-  async componentDidMount() {
-    const feedId = parseInt(this.props.match.params.feedId);
-    const feed = await getFeed(feedId);
-    this.setState({
-      remoteData: {
-        type: "SUCCESS",
-        data: feed
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.remoteData.type === "SUCCESS" && (
-          <Feed {...this.state.remoteData.data} />
-        )}
-      </div>
-    );
-  }
-}
+export const FeedContainer: React.SFC<Props> = ({ match }) => (
+  <Feed feedId={parseInt(match.params.feedId)} />
+);
