6f9b5c20c8fac196a5391e541396a41b7ef78f15
Shane Drury <shane.drury@freeagent.com>
2018-05-09 19:24:01 +0100

    Add episode name to GlobalPlayer

--- app/javascript/app/components/GlobalPlayer.tsx
+++ app/javascript/app/components/GlobalPlayer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getEpisodes } from "../modules/episodes/selectors";
 import {
   getPlayedSeconds,
   getPlayingEpisode,
 } from "../modules/player/selectors";
 import { RootState } from "../modules/reducers";
+import { RemoteEpisode } from "../types/episode";
 import Player from "./Player";
 
 interface DataProps {}
 
 interface PropsExtended {
+  episode?: RemoteEpisode;
   episodeId?: number;
   playedSeconds: number;
 }
@@ -21,6 +24,7 @@ export class GlobalPlayer extends React.PureComponent<Props> {
     return (
       <div>
         {this.props.episodeId && <Player episodeId={this.props.episodeId} />}
+        {this.props.episode && <div>{this.props.episode.name}</div>}
       </div>
     );
   }
@@ -29,8 +33,10 @@ export class GlobalPlayer extends React.PureComponent<Props> {
 const mapStateToProps = (state: RootState): PropsExtended => {
   const episodeId = getPlayingEpisode(state);
   const playedSeconds = episodeId && getPlayedSeconds(state)[episodeId];
+  const episode = episodeId ? getEpisodes(state)[episodeId] : undefined;
 
   return {
+    episode,
     episodeId,
     playedSeconds: playedSeconds ? playedSeconds : 0,
   };
