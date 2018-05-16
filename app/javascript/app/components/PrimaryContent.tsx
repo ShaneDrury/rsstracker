865034bd5002392296fe4f76f1fd37cfdf145133
Shane Drury <shane.drury@freeagent.com>
2018-05-16 21:11:01 +0100

    Add basic mobile support

--- app/javascript/app/components/PrimaryContent.tsx
+++ app/javascript/app/components/PrimaryContent.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 import { Route } from "react-router";
 import { Link } from "react-router-dom";
@@ -8,41 +9,78 @@ import Feeds from "./Feeds";
 import GlobalPlayer from "./GlobalPlayer";
 import UpdateFeeds from "./UpdateFeeds";
 
-export const PrimaryContent: React.SFC = () => (
-  <div>
-    <nav
-      className="navbar is-fixed-top has-background-light"
-      role="navigation"
-      aria-label="dropdown navigation"
-    >
-      <div className="navbar-start">
-        <div className="navbar-item">
-          <Link to="/">Feed Tracker</Link>
-        </div>
-        <div className="navbar-item has-dropdown is-hoverable">
-          <div className="navbar-link">Feeds</div>
-          <Feeds />
-        </div>
-        <div className="navbar-item">
-          <UpdateFeeds />
-        </div>
-        <div className="navbar-item">
-          <ActiveJobs />
-        </div>
+interface Props {}
+
+interface State {
+  isOpen: boolean;
+}
+
+export class PrimaryContent extends React.Component<Props, State> {
+  public state = {
+    isOpen: false,
+  };
+
+  public handleToggleOpen = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  public render() {
+    return (
+      <div>
+        <nav
+          className="navbar is-fixed-top has-background-light"
+          role="navigation"
+          aria-label="dropdown navigation"
+        >
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              Feed Tracker
+            </Link>
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              onClick={this.handleToggleOpen}
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+            </a>
+          </div>
+          <div
+            className={classNames("navbar-menu", {
+              "is-active": this.state.isOpen,
+            })}
+          >
+            <div className="navbar-start">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <div className="navbar-link">Feeds</div>
+                <Feeds />
+              </div>
+              <div className="navbar-item">
+                <UpdateFeeds />
+              </div>
+              <div className="navbar-item">
+                <ActiveJobs />
+              </div>
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item is-right is-paddingless">
+                <GlobalPlayer />
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Route exact path="/" component={AllFeeds} />
+              <Route path="/:feedId" component={Feed} />
+            </div>
+          </div>
+        </section>
       </div>
-      <div className="navbar-end">
-        <div className="navbar-item is-right is-paddingless">
-          <GlobalPlayer />
-        </div>
-      </div>
-    </nav>
-    <section className="section">
-      <div className="columns">
-        <div className="column">
-          <Route exact path="/" component={AllFeeds} />
-          <Route path="/:feedId" component={Feed} />
-        </div>
-      </div>
-    </section>
-  </div>
-);
+    );
+  }
+}
