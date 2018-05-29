e134e1cee60fd9e460767667628dffa3d79b41e9
Shane Drury <shane.drury@freeagent.com>
2018-05-29 18:12:49 +0100

    DRY up sync query params a bit

--- app/javascript/app/middleware/syncQueryParams.ts
+++ app/javascript/app/middleware/syncQueryParams.ts
@@ -1,29 +1,30 @@
+import * as qs from "qs";
 import { Middleware } from "redux";
 import {
   episodeActions as actions,
   EpisodesAction,
 } from "../modules/episodes/actions";
+import { QueryParams } from "../modules/location/queryParams";
 import { getQueryParams } from "../modules/location/selectors";
-import { updateQueryParams } from "../modules/queryParams";
 import { history } from "../store";
 
 const syncQueryParams: Middleware = store => next => (
   action: EpisodesAction
 ) => {
-  if (action.type === actions.CHANGE_FILTER) {
+  if (
+    action.type === actions.CHANGE_FILTER ||
+    action.type === actions.CHANGE_SEARCH
+  ) {
     const state = store.getState();
     const params = getQueryParams(state);
-    const newQueryParams = updateQueryParams(params, {
-      filter: action.payload.filter,
-    });
-    history.push({ search: `?${newQueryParams}` });
-  }
-  if (action.type === actions.CHANGE_SEARCH) {
-    const state = store.getState();
-    const params = getQueryParams(state);
-    const newQueryParams = updateQueryParams(params, {
-      searchTerm: action.payload.searchTerm,
-    });
+    const newParams: QueryParams = {};
+    if (action.type === actions.CHANGE_FILTER) {
+      newParams.filter = action.payload.filter;
+    }
+    if (action.type === actions.CHANGE_SEARCH) {
+      newParams.searchTerm = action.payload.searchTerm;
+    }
+    const newQueryParams = qs.stringify({ ...params, ...newParams });
     history.push({ search: `?${newQueryParams}` });
   }
   return next(action);
--- app/javascript/app/modules/queryParams.ts
+++
@@ -1,9 +0,0 @@
-import * as qs from "qs";
-import { QueryParams } from "./location/queryParams";
-
-export const updateQueryParams = (
-  previous: QueryParams,
-  changes: { [key: string]: string }
-) => {
-  return qs.stringify({ ...previous, ...changes });
-};
