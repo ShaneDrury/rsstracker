410a82e27de282aab7f083d020b0f48be50978ab
Shane Drury <shane.drury@freeagent.com>
2018-06-16 19:25:44 +0100

    Show jobs as notifications

--- app/javascript/app/components/ActiveJobs.tsx
+++ app/javascript/app/components/ActiveJobs.tsx
@@ -1,4 +1,7 @@
+import { difference } from "lodash";
 import React from "react";
+import Notification from "react-bulma-notification";
+import "react-bulma-notification/build/css/index.css";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchJobs, JobsAction } from "../modules/jobs/actions";
@@ -17,19 +20,46 @@ interface DispatchProps {
 
 type Props = EnhancedProps & DispatchProps;
 
-export class ActiveJobs extends React.PureComponent<Props> {
+interface State {
+  keys: string[];
+}
+
+export class ActiveJobs extends React.PureComponent<Props, State> {
+  public state: State = {
+    keys: [],
+  };
+
   public componentDidMount() {
     this.props.getJobs();
   }
 
+  public componentDidUpdate(prevProps: Props, prevState: State) {
+    if (prevProps.jobs !== this.props.jobs) {
+      const keysToRemove = difference(
+        prevState.keys,
+        this.props.jobs.map(job => job.id)
+      );
+      keysToRemove.forEach(key => {
+        Notification.remove(key);
+      });
+      const keys = this.props.jobs.map(job => {
+        const key = job.id;
+        if (!this.state.keys.includes(key)) {
+          Notification.info(job.jobData.jobClass, {
+            key,
+            duration: 0,
+            closable: false,
+            placement: "bottomLeft",
+          });
+        }
+        return key;
+      });
+      this.setState({ keys });
+    }
+  }
+
   public render() {
-    return (
-      <div>
-        {this.props.jobs.map(job => (
-          <span key={job.key}>{job.jobData.jobClass}&nbsp;</span>
-        ))}
-      </div>
-    );
+    return null;
   }
 }
 
