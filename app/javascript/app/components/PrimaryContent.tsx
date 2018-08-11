5e8ad4aaaec4a114db2b185e65b69c74445cbd81
Shane Drury <shane.drury@freeagent.com>
2018-08-11 16:29:04 +0100

    Remove react-router direct dependency

--- app/javascript/app/components/PrimaryContent.tsx
+++ app/javascript/app/components/PrimaryContent.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route, RouteComponentProps } from "react-router-dom";
 import AllFeeds from "./AllFeeds";
 import Feed from "./Feed";
 import { Navbar } from "./Navbar";
 
 class PrimaryContent extends React.PureComponent {
+  public renderFeed = ({ match }: RouteComponentProps<{ feedId: string }>) => (
+    <Feed feedId={match.params.feedId} />
+  );
+
   public render() {
     return (
       <div>
@@ -12,8 +16,8 @@ class PrimaryContent extends React.PureComponent {
         <section className="section">
           <div className="columns">
             <div className="column">
+              <Route path="/:feedId" render={this.renderFeed} />
               <Route exact path="/" component={AllFeeds} />
-              <Route path="/:feedId" component={Feed} />
             </div>
           </div>
         </section>
