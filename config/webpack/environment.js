ffc2852a2b523073ea7950ab8e34450bb9f98eb5
Shane Drury <shane.drury@freeagent.com>
2018-09-08 11:15:13 +0100

    Reduce bundle size a bit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,17 @@
+const webpack = require('webpack')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const { environment } = require('@rails/webpacker')
 const babelLoader = environment.loaders.get("babel")
 babelLoader.test = /\.(js|jsx|ts|tsx)?(\.erb)?$/
+
+environment.plugins.prepend(
+  'IgnoreMoment',
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+)
+
+environment.plugins.prepend(
+  'LodashReplacement',
+  new LodashModuleReplacementPlugin
+)
 
 module.exports = environment
