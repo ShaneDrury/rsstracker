43b5caff9cd7a6feacf98b78b6915ad318333095
Shane Drury <shane.drury@freeagent.com>
2018-09-23 12:01:18 +0100

    Add button to remove favourite

--- app/javascript/app/components/FavouriteItem.tsx
+++ app/javascript/app/components/FavouriteItem.tsx
@@ -1,10 +1,13 @@
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import classnames from "classnames";
 import React from "react";
 import { connect } from "react-redux";
 import { detailsOpened } from "../modules/episodes/actions";
 import { getDetailEpisodeId } from "../modules/episodes/selectors";
+import { favouriteRemoved } from "../modules/favourites/actions";
 import { RootState } from "../modules/reducers";
 import { RemoteEpisode } from "../types/episode";
+import { Icon } from "./Icon";
 
 interface DataProps {
   episode: RemoteEpisode;
@@ -16,6 +19,7 @@ interface EnhancedProps {
 
 interface DispatchProps {
   handleDetailOpened: (episodeId: string) => void;
+  onRemoveFavourite: (episodeId: string) => void;
 }
 
 type Props = DataProps & EnhancedProps & DispatchProps;
@@ -24,16 +28,23 @@ class FavouriteItem extends React.PureComponent<Props> {
   public handleDetailOpened = () => {
     this.props.handleDetailOpened(this.props.episode.id);
   };
+
+  public handleRemoveFavourite = () => {
+    this.props.onRemoveFavourite(this.props.episode.id);
+  };
+
   public render() {
     return (
-      <a
+      <div
         className={classnames("panel-block", {
           "is-active": this.props.isActive,
         })}
-        onClick={this.handleDetailOpened}
       >
-        {this.props.episode.name}
-      </a>
+        <a onClick={this.handleDetailOpened}>{this.props.episode.name}</a>
+        <button className="button" onClick={this.handleRemoveFavourite}>
+          <Icon icon={faWindowClose} />
+        </button>
+      </div>
     );
   }
 }
@@ -50,6 +61,7 @@ const mapStateToProps = (
 
 const mapDispatchToProps = {
   handleDetailOpened: detailsOpened,
+  onRemoveFavourite: favouriteRemoved,
 };
 
 export default connect(
