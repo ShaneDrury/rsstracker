5c06f85bd7e7d17fbb35516e1cff86b1e9508bfd
Shane Drury <shane.drury@freeagent.com>
2018-09-24 14:56:11 +0100

    Let DateField use EditMode component

--- app/javascript/app/components/DateField.tsx
+++ app/javascript/app/components/DateField.tsx
@@ -2,6 +2,7 @@ import * as moment from "moment";
 import React from "react";
 import { connect } from "react-redux";
 import { saveDate } from "../modules/episodes/actions";
+import { EditMode, EditModeOff, EditModeOn } from "./EditMode";
 
 interface DataProps {
   episodeId: string;
@@ -14,59 +15,38 @@ interface DispatchProps {
 
 type Props = DataProps & DispatchProps;
 
-interface State {
-  editMode: boolean;
-  pendingChanges: string;
-}
-
-export class DateField extends React.PureComponent<Props, State> {
-  public state = {
-    editMode: false,
-    pendingChanges: moment(this.props.date).format("YYYY-MM-DD"),
+export class DateField extends React.PureComponent<Props> {
+  public handleSave = (changes: string) => {
+    this.props.saveDate(this.props.episodeId, changes);
   };
 
-  public editModeOn = () => {
-    this.setState({ editMode: true });
-  };
+  public editModeOff: EditModeOff = ({ value, startEditing }) => (
+    <span onClick={startEditing}>{moment(value).format("ll")}</span>
+  );
 
-  public cancelEditing = () => {
-    this.setState({ editMode: false });
-  };
-
-  public handleSave = () => {
-    this.props.saveDate(this.props.episodeId, this.state.pendingChanges);
-    this.setState({ editMode: false });
-  };
-
-  public handleEdit: React.ChangeEventHandler<HTMLInputElement> = event => {
-    this.setState({ pendingChanges: event.target.value });
-  };
-
-  public clearEdits = () => {
-    this.setState({ pendingChanges: this.props.date });
-  };
+  public editModeOn: EditModeOn = ({ value, onEdit, onCancel, onSave }) => (
+    <span>
+      <input
+        autoFocus
+        onChange={onEdit}
+        onBlur={onCancel}
+        type="date"
+        value={value}
+      />
+      <a className="button is-primary" onMouseDown={onSave}>
+        Save
+      </a>
+    </span>
+  );
 
   public render() {
-    if (!this.state.editMode) {
-      return (
-        <span onClick={this.editModeOn}>
-          {moment(this.props.date).format("ll")}
-        </span>
-      );
-    }
     return (
-      <span>
-        <input
-          autoFocus
-          onChange={this.handleEdit}
-          onBlur={this.cancelEditing}
-          type="date"
-          value={this.state.pendingChanges}
-        />
-        <a className="button is-primary" onMouseDown={this.handleSave}>
-          Save
-        </a>
-      </span>
+      <EditMode
+        initialValue={moment(this.props.date).format("YYYY-MM-DD")}
+        onSave={this.handleSave}
+        editModeOff={this.editModeOff}
+        editModeOn={this.editModeOn}
+      />
     );
   }
 }
