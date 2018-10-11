ffca101a52d834e3dbd787e56e6f1dc26a462bb8
Shane Drury <shane.drury@freeagent.com>
2018-10-11 14:38:54 +0100

    Try namespacing class

--- app/jobs/download_youtube_playlist_job.rb
+++ app/jobs/download_youtube_playlist_job.rb
@@ -2,7 +2,7 @@ class DownloadYoutubePlaylistJob < ApplicationJob
   queue_as :default
 
   def perform(feed_id)
-    downloader = YoutubePlaylistDownloader.new(feed_id, Rails.application.config.youtube_dl_path)
+    downloader = ::YoutubePlaylistDownloader.new(feed_id, Rails.application.config.youtube_dl_path)
     downloader.download_playlist
   end
 end
