b67d8c6c69999b54c9c2f62dd4f01a8eb0aa6d2e
Shane Drury <shane.drury@freeagent.com>
2018-10-11 21:13:00 +0100

    Scope thumbnail job

--- app/jobs/download_thumbnail_job.rb
+++ app/jobs/download_thumbnail_job.rb
@@ -1,8 +1,10 @@
+require "thumbnail_downloader"
+
 class DownloadThumbnailJob < ApplicationJob
   queue_as :default
 
   def perform(episode_id)
-    downloader = ThumbnailDownloader.new(episode_id, Rails.application.config.download_root, Rails.application.config.storage_root)
+    downloader = ::ThumbnailDownloader.new(episode_id, Rails.application.config.download_root, Rails.application.config.storage_root)
     downloader.download
   end
 end
