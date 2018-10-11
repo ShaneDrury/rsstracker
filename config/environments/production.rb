484638e3e389023e898d1d1480211999f1e0af38
Shane Drury <shane.drury@freeagent.com>
2018-10-11 09:20:09 +0100

    Restore action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
