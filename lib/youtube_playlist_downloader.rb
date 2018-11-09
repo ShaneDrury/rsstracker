a5e39a339a29bf17d626e169274a6151ed57367e
Shane Drury <shane.drury@freeagent.com>
2018-11-09 08:34:49 +0000

    Set feed and name

--- lib/youtube_playlist_downloader.rb
+++ lib/youtube_playlist_downloader.rb
@@ -21,9 +21,12 @@ class YoutubePlaylistDownloader
 
   def update_or_create_episode(episode)
     url = episode['url']
+    description = episode['title']
     updater = ::YoutubeEpisodeUpdater.new(youtube_dl_path)
     Episode.find_or_create_by(guid: url) do |ep|
       ep.build_fetch_status(status: 'NOT_ASKED')
+      ep.feed = feed
+      ep.name = description
       begin
         updater.update(ep, url)
       rescue IOError
