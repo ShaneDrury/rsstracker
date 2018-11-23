f8c7c629d4d3133d929b1f9be564bdc826ca542c
Shane Drury <shane.drury@freeagent.com>
2018-11-23 14:38:39 +0000

    Fix to use job_data

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,6 +2,6 @@ ActiveRecord::Base.logger.level = 1
 
 class Delayed::Job
   def with_extra
-    attributes.merge(job_data: payload_object)
+    attributes.merge(job_data: payload_object.job_data)
   end
 end
