0ed1ba6b7957826b9fc5fdfbc7f4642c3f50456d
Shane Drury <shane.drury@freeagent.com>
2019-05-06 20:43:38 +0100

    Fix short episode details key

--- lib/youtube.rb
+++ lib/youtube.rb
@@ -11,7 +11,7 @@ class Youtube
   def short_details(url)
     out = with_youtube_dl('--flat-playlist', '-j', '--', url)
     json_out = out.split("\n").map { |line| JSON.parse(line) }
-    ShortEpisodeDetails.new(json_out[:url], json_out[:title])
+    ShortEpisodeDetails.new(json_out["url"], json_out["title"])
   end
 
   def details(url)
