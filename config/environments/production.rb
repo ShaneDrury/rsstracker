4330369a6d8105e908a026815de1dc4391538045
Shane Drury <shane.drury@freeagent.com>
2019-05-23 09:25:49 +0100

    Disable lograge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   ActiveModelSerializers.config.adapter = :json_api
   ActiveModelSerializers.config.key_transform = :camel_lower
 
-  config.lograge.enabled = true
+  config.lograge.enabled = false
   config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.logstash.host = "ubuntu.home"
   config.logstash.port = 5000
