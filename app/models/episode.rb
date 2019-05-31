389564d7d7b47c48031e80641a62fcebed6d0d9a
Shane Drury <shane.drury@freeagent.com>
2019-05-31 11:41:23 +0100

    Try to avoid hitting storage during API call

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -36,7 +36,7 @@ class Episode < ApplicationRecord
   end
 
   def audio_url
-    File.join(Rails.application.config.storage_root, "shrine", audio_attachment.audio_url) if audio_attachment&.audio&.exists?
+    File.join(Rails.application.config.storage_root, "shrine", audio_attachment.audio_url) if audio_attachment&.audio_url&.present?
   end
 
   def should_download?
