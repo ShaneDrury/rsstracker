6251d29a1d055694352ff896aa3d8731cae40a7f
Shane Drury <shane.drury@freeagent.com>
2019-07-23 12:21:44 +0100

    Render into multiple roots

--- app/javascript/app/components/PrimaryContent.tsx
+++ app/javascript/app/components/PrimaryContent.tsx
@@ -11,105 +11,80 @@ import EpisodeDetail from "./EpisodeDetail";
 import EpisodeLoader from "./EpisodeLoader";
 import Episodes from "./Episodes";
 import EpisodesLoader from "./EpisodesLoader";
-import Favourites from "./Favourites";
 import FeedSelect from "./FeedSelect";
-import { Navbar } from "./Navbar";
 
 const parseLocation = (location: Location): QueryParams =>
   qs.parse(location.search, {
     ignoreQueryPrefix: true,
   });
 
-const Wrapper = styled.div`
-  display: flex;
-  height: calc(100vh - 74px);
-`;
-
-const Sidebar = styled.section`
-  flex: none;
-`;
-
-const Content = styled.section`
-  flex: 1;
-`;
-
-const Aside = styled.aside`
-  flex: 1;
-`;
-
 const Scrollable = styled.div`
   height: 100%;
   overflow: auto;
 `;
 
-const Main = styled.main`
-  height: 100%;
-`;
-
-interface EnhancedProps {
-  detailEpisodeId?: string;
-}
-
-type Props = EnhancedProps;
-
-class PrimaryContent extends React.Component<Props> {
-  public handleChangePage = (queryParams: QueryParams, history: History) => (
-    selected: number
-  ) => {
-    syncQueryParams({ currentPage: selected }, queryParams, history);
-  };
-
-  public renderRoot = ({
+export const FeedSelectContainer = () => {
+  const renderRoot = ({
     history,
     location,
     match,
   }: RouteComponentProps<{ feedId?: string }>) => {
     const queryParams = parseLocation(location);
     return (
-      <Main>
-        <Navbar />
-        <Wrapper>
-          <Sidebar>
-            <FeedSelect
-              queryParams={queryParams}
-              feedId={match.params.feedId}
-              history={history}
-            />
-            <Favourites />
-          </Sidebar>
-          <Content>
-            <EpisodesLoader
-              queryParams={queryParams}
-              feedId={match.params.feedId}
-            >
-              <Scrollable>
-                <Episodes
-                  handleChangePage={this.handleChangePage(queryParams, history)}
-                />
-              </Scrollable>
-            </EpisodesLoader>
-          </Content>
-          <Aside>
-            {this.props.detailEpisodeId && (
-              <Scrollable>
-                <EpisodeLoader episodeId={this.props.detailEpisodeId}>
-                  {remoteEpisode => <EpisodeDetail episode={remoteEpisode} />}
-                </EpisodeLoader>
-              </Scrollable>
-            )}
-          </Aside>
-        </Wrapper>
-      </Main>
+      <FeedSelect
+        queryParams={queryParams}
+        feedId={match.params.feedId}
+        history={history}
+      />
     );
   };
 
-  public render() {
-    return <Route path="/:feedId?" render={this.renderRoot} />;
-  }
+  return <Route path="/:feedId?" render={renderRoot} />;
+};
+
+export const EpisodesContainer = () => {
+  const handleChangePage = (queryParams: QueryParams, history: History) => (
+    selected: number
+  ) => {
+    syncQueryParams({ currentPage: selected }, queryParams, history);
+  };
+
+  const renderRoot = ({
+    history,
+    location,
+    match,
+  }: RouteComponentProps<{ feedId?: string }>) => {
+    const queryParams = parseLocation(location);
+    return (
+      <EpisodesLoader queryParams={queryParams} feedId={match.params.feedId}>
+        <Scrollable>
+          <Episodes handleChangePage={handleChangePage(queryParams, history)} />
+        </Scrollable>
+      </EpisodesLoader>
+    );
+  };
+  return <Route path="/:feedId?" render={renderRoot} />;
+};
+
+interface Props {
+  detailEpisodeId?: string;
 }
 
-const mapStateToProps = (state: RootState): EnhancedProps => ({
+export const Episode = ({ detailEpisodeId }: Props) => {
+  if (detailEpisodeId) {
+    return (
+      <Scrollable>
+        <EpisodeLoader episodeId={detailEpisodeId}>
+          {remoteEpisode => <EpisodeDetail episode={remoteEpisode} />}
+        </EpisodeLoader>
+      </Scrollable>
+    );
+  }
+  return null;
+};
+
+const mapStateToProps = (state: RootState): Props => ({
   detailEpisodeId: getDetailEpisodeId(state),
 });
 
-export default connect(mapStateToProps)(PrimaryContent);
+export const EpisodeContainer = connect(mapStateToProps)(Episode);
--- app/javascript/app/index.tsx
+++ app/javascript/app/index.tsx
@@ -3,8 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import { BrowserRouter, Route } from "react-router-dom";
-import PrimaryContent from "./components/PrimaryContent";
+import { Router, Route } from "react-router-dom";
+import {
+  EpisodeContainer,
+  EpisodesContainer,
+  FeedSelectContainer,
+} from "./components/PrimaryContent";
 import { fetchFeedsComplete } from "./modules/feeds/actions";
 import { processFeed } from "./modules/feeds/sources";
 import { getPreloaded } from "./modules/getInitialState";
@@ -13,6 +17,9 @@ import { processJobResponse } from "./modules/jobs/sources";
 import sagas from "./modules/sagas";
 import { configureStore } from "./store";
 import { init } from "./websocket";
+import { Navbar } from "./components/Navbar";
+import Favourites from "./components/Favourites";
+import { createBrowserHistory } from "history";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
@@ -23,25 +30,34 @@ if (process.env.NODE_ENV === "production") {
 const { store, runSaga } = configureStore();
 runSaga(sagas);
 init(store);
+const history = createBrowserHistory();
 const preloaded = getPreloaded();
 store.dispatch(fetchFeedsComplete(preloaded.feeds.data.map(processFeed)));
 store.dispatch(fetchJobsComplete(preloaded.jobs.data.map(processJobResponse)));
 
-const render = (Component: React.ComponentType) => {
+const rootWrap = (Component: React.ComponentType, el: HTMLElement | null) => {
   return ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Route component={Component} />
-      </BrowserRouter>
+      </Router>
     </Provider>,
-    document.getElementById("root")
+    el
   );
 };
 
-render(PrimaryContent);
+const renderRoots = () => {
+  rootWrap(Navbar, document.getElementById("navbar"));
+  rootWrap(FeedSelectContainer, document.getElementById("feed-select"));
+  rootWrap(Favourites, document.getElementById("favourites"));
+  rootWrap(EpisodesContainer, document.getElementById("content"));
+  rootWrap(EpisodeContainer, document.getElementById("aside"));
+};
+
+renderRoots();
 
 if (module.hot) {
   module.hot.accept("./components/PrimaryContent", () => {
-    render(PrimaryContent);
+    renderRoots();
   });
 }
