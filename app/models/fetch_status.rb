11cdf39729ecd31fe8d2e3e704e7b0e011248af1
Shane Drury <shane.drury@freeagent.com>
2019-09-30 09:06:21 +0100

    Remove unused FetchStatus method

--- app/models/fetch_status.rb
+++ app/models/fetch_status.rb
@@ -1,8 +1,3 @@
 class FetchStatus < ApplicationRecord
   belongs_to :fetchable, polymorphic: true, touch: true
-  include ActionView::Helpers::NumberHelper
-
-  def percentage_fetched
-    number_with_precision(bytes_transferred.to_d / bytes_total.to_d * 100.0, precision: 2) if status == 'LOADING' && bytes_transferred.present?
-  end
 end
