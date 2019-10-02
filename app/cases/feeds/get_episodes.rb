149bf3b5dae4a9951dc52994f978c6644d52c370
Shane Drury <shane.drury@freeagent.com>
2019-10-02 17:23:25 +0100

    Correct use of create_episode

--- app/cases/feeds/get_episodes.rb
+++ app/cases/feeds/get_episodes.rb
@@ -7,7 +7,7 @@ module Feeds
     def run
       feed.new_episodes.each do |remote_episodes|
         remote_episodes.each do |source, remote_episode|
-          episode = feed.create_episode(source, remote_episode)
+          episode = feed.episodes.create_episode(source, remote_episode)
           next unless episode.should_download?
 
           DownloadThumbnailJob.perform_later(episode.id) if episode.thumbnail_url
