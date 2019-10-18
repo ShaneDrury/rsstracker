ed3d3504552237bf284135aad3ca337e611ea08c
Shane Drury <shane.drury@freeagent.com>
2019-10-18 15:27:46 +0100

    Remove unused jobs

--- app/jobs/mark_loading_eps_as_failed_job.rb
+++
@@ -1,8 +0,0 @@
-class MarkLoadingEpsAsFailedJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    eps = Episode.joins(:fetch_status).where(fetch_statuses: { status: "LOADING" })
-    eps.map { |ep| ep.build_fetch_status(status: "FAILURE").save }
-  end
-end
--- app/jobs/retry_failed_eps_job.rb
+++
@@ -1,8 +0,0 @@
-class RetryFailedEpsJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    eps = Episode.joins(:fetch_status).where(fetch_statuses: { status: "FAILURE" })
-    eps.map { |ep| DownloadEpisodeJob.perform_later(ep.id) }
-  end
-end
