eade346c695958801b35e3d9c26915b4d645eb59
Shane Drury <shane.drury@freeagent.com>
2019-10-22 19:39:41 +0100

    Move download_new to source

--- app/jobs/update_source_episodes_job.rb
+++ app/jobs/update_source_episodes_job.rb
@@ -2,8 +2,6 @@ class UpdateSourceEpisodesJob < ApplicationJob
   queue_as :default
 
   def perform(source_id)
-    source = Source.find(source_id)
-
-    RemoteEpisodes.new(source).download_new
+    Source.find(source_id).download_new
   end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -26,6 +26,22 @@ class Source < ApplicationRecord
     end
   end
 
+  def download_new
+    with_update do
+      episodes_with_feed.each do |remote_episode|
+        episode = remote_episode.create_episode
+        # Move stuff after this to episode?
+        # download_if_needed
+        next unless episode.should_download?
+
+        DownloadThumbnailJob.perform_later(episode.id) if episode.source_thumbnail_url
+        DownloadRemoteAudioJob.perform_later(episode.id) if remote_episode.feed.autodownload
+      end
+    end
+  end
+
+  private
+
   def new_episodes
     guids = remote_episodes.map(&:url)
     # Bit dodgy, maximum 32k values here in the where(guid: guids)
@@ -35,14 +51,16 @@ class Source < ApplicationRecord
     remote_episodes.select { |episode| new_guids.include?(episode.url) }
   end
 
+  def episodes_with_feed
+    new_episodes.select { |episode| episode.feed.present? }
+  end
+
   def with_update
     SourceUpdateStartedBroadcastJob.perform_later(id)
     result = yield
     SourceUpdateCompleteBroadcastJob.perform_later(id)
     result
   end
-
-  private
 
   def remote_episodes
     @remote_episodes ||= remote.episodes
